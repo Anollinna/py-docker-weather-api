21e8d4d9b6bef69212656441294f54040abff669
Khrystyna Sen <anollina@ukr.net>
2025-05-08 09:19:28 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,6 +1,23 @@
+import requests
+import os
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    API_KEY = os.getenv("API_KEY")
+
+    if not API_KEY:
+        raise Exception("API_KEY not found in environment")
+
+    CITY = "Paris"
+    URL = f"https://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+    response = requests.get(URL)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(f"Current temperature in {CITY} is {data['main']['temp']}°C")
+    else:
+        print(f"Failed to get weather data for {CITY}")
 
 
 if __name__ == "__main__":
